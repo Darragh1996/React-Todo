8f3b98c46ea8aa567afd728ab1414a5b3b3fcb72
Darragh Ferry <darraghferry@hotmail.com>
2019-12-02 12:58:06 +0000

    implemented a handleChange method in App.js and passed this method to the TodoForm component as a prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from "react";
+import TodoForm from "./components/TodoComponents/TodoForm";
+// import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  handleChange = event => {
+    console.log(event.target.value);
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      <>
+        {/* <TodoList /> */}
+        <TodoForm handleChange={this.handleChange} />
+      </>
     );
   }
 }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class TodoForm extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <input value={this.props.todo} onChange={this.props.handleChange} />
+        <button>submit</button>
+      </form>
+    );
+  }
+}
